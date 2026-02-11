c5a8cec3ac35526fdefaf38c2755ee3b20496f98
dennislacara <lacaradennis@gmail.com>
2026-02-11 22:50:30 +0100

    modifica 2 febbraio 2026

--- main.py
+++ main.py
@@ -18,5 +18,5 @@ def main(page: ft.Page):
 ft.app(
     target=main,
     host="0.0.0.0",
-    port=int(os.environ.get("dennis_prova", 22082004)),
+    port=int(os.environ.get("dennis_prova", 0-65535)),
 )
